466a7b50f9052245e3cb2ab5147f27b23379495f
hu970804 <812510003@qq.com>
2019-08-09 20:34:53 +0800

    add interface.ts

---
+++ src/interface.ts
@@ -0,0 +1,38 @@
+interface List {
+  readonly id: number;
+  name: string,
+  age ?: number
+}
+
+interface Result {
+  data: List[]
+}
+
+function render(result: Result) {
+  result.data.forEach((value) => {
+    console.log(value.id, value.name);
+    if (value.age) {
+      console.log(value.age);
+    }
+  })
+}
+
+let result = {
+  data: [
+    {id: 1, name: 'A', sex: 'male'},
+    {id: 2, name: 'B', age: 10}
+  ]
+}
+
+render(result)
+
+interface StringArray {
+  [index: number]: string
+}
+let chars: StringArray = ['A', 'B']
+
+interface Names {
+  [x: string] : string;
+  [z: number] : string
+}
+
